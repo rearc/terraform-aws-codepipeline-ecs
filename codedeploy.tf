a9e32ed9e09677ce1bb7e232c652722077b68686
Michael Kaufman <mwkaufman@gmail.com>
2020-06-17 00:28:58 -0400

    Add support for tasks and services without load balancer

--- codedeploy.tf
+++ codedeploy.tf
@@ -7,50 +7,62 @@ resource "aws_codedeploy_app" "codedeploy" {
 
 
 resource "aws_codedeploy_deployment_group" "deployment_group" {
-  count                  = contains(var.pipeline_types,"build-unit-deploy") || contains(var.pipeline_types,"build-deploy") ? 1 : 0
+  count                  = contains(var.pipeline_types,"build-unit-deploy") || contains(var.pipeline_types,"build-deploy") || contains(var.pipeline_types,"deploy")  ? 1 : 0
   app_name               = aws_codedeploy_app.codedeploy[0].name
   deployment_group_name  = "${var.app_name}_${local.stack}_deployment_group"
   service_role_arn       = "${aws_iam_role.codedeploy_iam_role.arn}"
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  blue_green_deployment_config {
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-
-    terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 5
-    }
-  }
-
-  deployment_style {
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type   = "BLUE_GREEN"
-  }
 
   ecs_service {
     cluster_name = "${var.ecs_cluster_name}"
     service_name = "${local.stack}_${var.app_name}"
   }
 
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = var.listener_arns
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  dynamic "blue_green_deployment_config" {
+    for_each = var.blue_target_group_arn == "" ? [] : [1]
+    
+    content {
+      deployment_ready_option {
+        action_on_timeout = "CONTINUE_DEPLOYMENT"
       }
 
-      target_group {
-        name = "${var.blue_target_group_arn}"
+      terminate_blue_instances_on_deployment_success {
+        action                           = "TERMINATE"
+        termination_wait_time_in_minutes = 5
       }
+    }
+  }
 
-      target_group {
-        name = "${var.green_target_group_arn}"
+  dynamic "deployment_style" {
+    for_each = var.blue_target_group_arn == "" ? [] : [1]
+
+    content {
+      deployment_option = "WITH_TRAFFIC_CONTROL"
+      deployment_type   = "BLUE_GREEN"
+    }
+  }
+
+  dynamic "load_balancer_info" {
+    for_each = var.blue_target_group_arn == "" ? [] : [1]
+
+    content {
+      target_group_pair_info {
+        prod_traffic_route {
+          listener_arns = var.listener_arns
+        }
+
+        target_group {
+          name = "${var.blue_target_group_arn}"
+        }
+
+        target_group {
+          name = "${var.green_target_group_arn}"
+        }
       }
     }
   }
